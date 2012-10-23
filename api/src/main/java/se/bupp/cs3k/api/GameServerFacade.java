4feace9ba620b080680168ef21df6e377b6d37cb
Kalle W <kalle.widell@gmail.com>
2012-10-23 03:02:19 +0200

    added log4j + some debug on gameserverfacade

--- api/src/main/java/se/bupp/cs3k/api/GameServerFacade.java
+++ api/src/main/java/se/bupp/cs3k/api/GameServerFacade.java
@@ -1,6 +1,8 @@
 package se.bupp.cs3k.api;
 
 import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,4 +14,12 @@ import java.rmi.RemoteException;
 public interface GameServerFacade {
 
     public AbstractPlayerInfo evaluateGamePass(String pass) throws RemoteException;
+
+    //public void setScore(Integer occassionId, String serializedScore) throws RemoteException;
+
+    public void startGame(Integer occassionId, Map<Integer,Integer> teamsByBlayers) throws RemoteException;
+
+    public void startGame(Integer occassionId, List<Integer> players) throws RemoteException;
+
+    public void endGame(Integer occassionId, String serializedScore) throws RemoteException;
 }
--- server/src/main/scala/se/bupp/cs3k/server/facade/GameServerFacadeImpl.scala
+++ server/src/main/scala/se/bupp/cs3k/server/facade/GameServerFacadeImpl.scala
@@ -4,6 +4,10 @@ import se.bupp.cs3k.api._
 import org.springframework.stereotype.Service
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.rmi.server.UnicastRemoteObject
+import java.lang.Integer
+import java.lang.{Integer => JInt}
+import org.apache.log4j.Logger
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,15 +19,31 @@ import java.rmi.server.UnicastRemoteObject
 @Service("gameServerFacade")
 class GameServerFacadeImpl() extends GameServerFacadeRemote with GameServerFacade {
 
+  val log = Logger.getLogger(classOf[GameServerFacadeImpl])
+
   val om = new ObjectMapper()
 
   @throws(classOf[java.rmi.RemoteException])
   def evaluateGamePass(pass: String) = {
+    log.info("evaluateGamePass invoked")
     var absPI = om.readValue(pass, classOf[AbstractGamePass])
     absPI match {
       case t:Ticket => new RegisteredPlayerInfo()
       case t:AnonymousPass => new AnonymousPlayerInfo(t.getName)
     }
+
+  }
+
+  def startGame(occassionId: JInt, teamsByBlayers: java.util.Map[JInt, JInt]) {
+    log.info("StartGame invoked")
+  }
+
+  def startGame(occassionId: JInt, players: java.util.List[JInt]) {
+
+  }
+
+  def endGame(occassionId: JInt, serializedScore: String) {
+    log.info("EndGame invoked")
 
   }
 }
